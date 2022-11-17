eca84b5dc0448532dc965c7125f16124e2527baa
Burak <burakuren101@gmail.com>
2022-11-17 19:09:38 +0300

    ready

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,19 +1,18 @@
 import pandas as pd
-from sqlalchemy import create_engine, update, delete, insert
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-import requests
 from django.http import JsonResponse
 
-engine = create_engine('sqlite:///data.db', echo = True)
+engine = create_engine('sqlite:///data.db', echo=True)
 session = Session(engine)
 
 csv_file = "~/proje/mysite/mysite/data.csv"
 
-table_name = "data"
+table_name = "data" # a name script that should solve the naming problem.
+
 df = pd.read_csv(csv_file)
 
 #Put it into db
-
 def create_table(request):
     df.to_sql(table_name, con=engine, schema=None, if_exists='append', index=False, index_label=None, chunksize=None,
               dtype=None, method=None)
@@ -23,13 +22,15 @@ def create_table(request):
 def read_table(request): # or csv file is gonna be transferred by filesystem.
     result = engine.execute(f"SELECT * FROM {table_name}").fetchall()
 
+    #If you wanna get results in any format you gotta do that here.
+
     return JsonResponse({"message":"Success"})
 
 #There is gonna be a POST request and where are gonna use that json to update assosicated columns and rows.
 def update_table(request):
     json_data = request.data
 
-    # SQL EXECUTIONS
+    # SQL EXECUTIONS...
     engine.execute()
     session.commit()
 
@@ -38,12 +39,16 @@ def update_table(request):
 def insert_value(request):
     json_data = request.data
 
-    # SQL EXECUTIONS
+    # SQL EXECUTIONS...
     engine.execute()
     session.commit()
-    return JsonResponse({"message":"Success"})
+    return JsonResponse({"message": "Success"})
 
 
 def delete_table():
 
-    delete()
+    # SQL EXECUTIONS...
+    engine.execute()
+    session.commit()
+
+    return JsonResponse({"message": "Success"})
